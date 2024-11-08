a122fcfca7fd11b448cc61a60b218e916fe0d96d
Pratiksha Gore <129929834+Pratiksha1302@users.noreply.github.com>
2024-11-08 22:59:47 +0530

    Add files via upload

--- config.php
+++ config.php
@@ -4,4 +4,9 @@ $dbpass="";
 $host="localhost";
 $db="hostel";
 $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
+
+// Base URL and image directory path
+/*define('BASE_URL', 'http://localhost/hostelsystem/hostel/'); // Change this URL according to your setup
+define('IMAGE_DIR', 'image/'); // Change this directory path according to your setup
+*/
 ?>
